949d60e50b4bb2567d2bb1aaf914adf706b957b9
Abdullah2100 <ali735501225@gmail.com>
2022-04-23 07:03:37 +0300

    fixing issuse

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,33 +7,47 @@ import { AuthModule } from './auth/auth.module';
 import { join } from 'path';
 import {ConfigModule, ConfigService} from '@nestjs/config'
 import { async } from 'rxjs';
+import { getEnvPath } from './common/helper/env.helper';
 
 
 
-
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 @Module({
   imports: [TasksModule,
     ConfigModule.forRoot({
-      envFilePath:[`${process.env.stage}.env `]
+      envFilePath:[envFilePath]
     }),
     TypeOrmModule.forRootAsync({
-     
-      imports:[ConfigModule]
-,inject:[ConfigService],
-useFactory:async (configService:ConfigService)=>{
-  return{
-    type:'postgres',
-    host: configService.get('host'),
-    port: configService.get(' port'),
-    username: configService.get('username'),
-    password: configService.get('password'),
-    database: configService.get('database'),
-    synchronize: true,
-    autoLoadEntities: true
-  }
-}
-    }),
-    AuthModule
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
+
+        return {
+          ssl:true,
+          type: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+          host: configService.get('DB_HOST'),
+          port: configService.get('port'),
+          username: configService.get('username'),
+          password: configService.get('password'),
+          database: configService.get('database'),
+        };
+      },
+    })
+//     .forRoot({
+//   type:'postgres',
+//   host:process.env.host,
+//   port:Number(process.env.port),
+//   username:process.env.username,
+//   password:process.env.password,
+//   database:process.env.database,
+//   synchronize: true,
+//   autoLoadEntities: true
+// }),
+    
+    ,AuthModule
   ],
  
 
